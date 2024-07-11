c0496905141b71eb22634f2b670072ad44aa51e9
angelica.kadja <angelicakadja@gmail.com>
2024-07-11 19:32:00 -0300

    Correção no código.

--- Criando-Exceções-No-Java/src/ExemploExcecao.java
+++ Criando-Exceções-No-Java/src/ExemploExcecao.java
@@ -3,15 +3,15 @@ import java.text.ParseException;
 
 public class ExemploExcecao {
     
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         Number valor;
-        
         try {        
             valor = NumberFormat.getInstance().parse("a1.75");
             System.out.println(valor);
-            
+
         } catch (ParseException e) {
+            e.printStackTrace();
         }
     }
 }
